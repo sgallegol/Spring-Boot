0ab8c89b484a2bf41663933cb6d802e7c0102a58
sgallegol <sgallegol@unal.edu.co>
2022-08-29 20:17:35 -0500

    REMOVE me thod, add newone

--- src/main/java/com/pruebanttdata/samir/controller/ControllerHome.java
+++ src/main/java/com/pruebanttdata/samir/controller/ControllerHome.java
@@ -3,6 +3,7 @@ package com.pruebanttdata.samir.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+<<<<<<< HEAD
 @Controller
 public class ControllerHome {
 	
@@ -14,5 +15,17 @@ public class ControllerHome {
 		
 	}
 	
+=======
+
+//prueba restablecimiento
+
+@Controller
+public class ControllerHome {
+	//http://localhost:80880
+	@GetMapping(value= "/")
+	public String home() {
+		return "home";
+	}
+>>>>>>> 48b690a86fd4436aa0d4d5c07986cb168423309d
 
 }
